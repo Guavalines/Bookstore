cd5eef2b8bfd43d156f9c12d38bbdccd438f36db
guavalines <konstrictor@gmail.com>
2023-03-02 22:41:52 -0800

    add use effect

--- frontend/src/features/posts/Posts.tsx
+++ frontend/src/features/posts/Posts.tsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useAppSelector } from "../../app/hooks";
-import { fetchPostAsync, selectPosts } from './postsSlice';
+import { fetchPostsAsync, selectPosts } from './postSlice';
 
 function Posts(){
   const posts = useAppSelector(selectPosts);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPostsAsync());
+  })
   return <div><h1>Posts</h1></div>;
 }
 
