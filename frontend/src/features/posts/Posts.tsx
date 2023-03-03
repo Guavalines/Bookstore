e6abe4a9fa66276946d2628af4963c896c0b8bc5
guavalines <konstrictor@gmail.com>
2023-03-02 22:28:01 -0800

    added import

--- frontend/src/features/posts/Posts.tsx
+++ frontend/src/features/posts/Posts.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
+import { fetchPostAsync, selectPosts } from './postsSlice';
+
 function Posts(){
+    const posts = useAppSelector(selectPosts);
   return <div><h1>Posts</h1></div>;
 }
 
