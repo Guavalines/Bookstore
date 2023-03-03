76f0f22f3243cdf8a6588ebe61316c29129f08ec
guavalines <konstrictor@gmail.com>
2023-03-02 22:52:45 -0800

    added status

--- frontend/src/features/posts/Posts.tsx
+++ frontend/src/features/posts/Posts.tsx
@@ -1,14 +1,20 @@
 import React, { useEffect } from 'react';
 import { useAppSelector } from "../../app/hooks";
-import { fetchPostsAsync, selectPosts } from './postSlice';
+import { fetchPostsAsync, selectPosts, Statuses } from './postSlice';
 
-function Posts(){
+function Posts() {
   const posts = useAppSelector(selectPosts);
+  const status = useAppSelector(selectStatus);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchPostsAsync());
-  })
+  }, [dispatch])
+
+  let contents;
+
+  if (status !== Statuses.upToDate) {}
+
   return <div><h1>Posts</h1></div>;
 }
 
