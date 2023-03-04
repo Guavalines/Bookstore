5aa56acf7be3a8cc4ef09d1987cfa45198626c38
guavalines <konstrictor@gmail.com>
2023-03-03 23:32:02 -0800

    fixed typo in store.ts and modified case in postSlice

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,6 +1,6 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
-import postsReducer from "../features/posts/postsSlice";
+import postsReducer from "../features/posts/postSlice";
 
 export const store = configureStore({
   reducer: {
