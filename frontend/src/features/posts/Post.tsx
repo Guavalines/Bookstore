f53c70c5d5b1cfff682fd7f71174577ffcef3133
guavalines <konstrictor@gmail.com>
2023-03-04 23:09:09 -0800

    added title and body elements

--- frontend/src/features/posts/Post.tsx
+++ frontend/src/features/posts/Post.tsx
@@ -3,6 +3,9 @@ import React from 'react';
 function Post(props:any){
     const [title, setTitle] = useState(props.post.title);
     const [body, setBody] = useState(props.post.body);
+
+    const titleElement = <h2 className="title text-start">{props.post.title}</h2>;
+    const bodyElement = <p className="card-text text-start">{props.post.body}</p>;
   return <div>Post</div>;
 }
 
