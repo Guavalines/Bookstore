bbcee735b9c97608a444d4f880a52a4a84763e87
guavalines <konstrictor@gmail.com>
2023-03-05 23:47:59 -0800

    added submit handler, reset state, and post form

--- frontend/src/features/posts/PostForm.tsx
+++ frontend/src/features/posts/PostForm.tsx
@@ -4,7 +4,27 @@ function PostForm(){
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
-  return <div>form</div>;
+
+  function submitHandler() {
+
+  }
+
+  function resetState() {
+    setTitle('');
+    setBody('');
+  }
+
+  return <div>
+    <h1>PostForm</h1>
+    <form
+      input="text"
+      className="form-control text-start"
+      name="title"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+      />
+    </form>
+  </div>;
 }
 
 export default PostForm;
