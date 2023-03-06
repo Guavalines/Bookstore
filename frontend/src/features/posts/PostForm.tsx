b0205678b97ee87bb9b9ddd64c4ef88092739b92
guavalines <konstrictor@gmail.com>
2023-03-05 23:32:43 -0800

    added dispatch and also use states

--- frontend/src/features/posts/PostForm.tsx
+++ frontend/src/features/posts/PostForm.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function PostForm(){
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
   return <div>form</div>;
 }
 
